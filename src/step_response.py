4ebcb692bbe95bb50cccab3dc56da848296cca4d
logdotzipp <loganfemling@gmail.com>
2024-01-10 19:22:04 -0800

    Doxygen Attempt 1
    Added comments to step_response.py
    
    Also modified the queue read loop slightly, needs to be tested
    
    Attempted to use doxygen, however something seems to not be working (index.html has no documentation...)

--- src/step_response.py
+++ src/step_response.py
@@ -6,19 +6,22 @@ micropython.alloc_emergency_exception_buf(100)
 
 
 """! @file main.py
-Doxygen style docstring for the file 
+   This program runs a 3.3V step on output pin A5, and then records the immediate response on pin PB0 for the following 2 seconds. Data points from the recorded response are printed over serial.
 """
 
+# Setup queue for optimized data storage in an interrupt
 QUEUE_SIZE = 200
 int_queue = cqueue.IntQueue(QUEUE_SIZE)
 
+# Enable adc pin
 adc = pyb.ADC(pyb.Pin.board.PB0)
 
+# Initialize doFillQueue to prevent the queue from filling immediately
 doFillQueue = False
 
 def timer_int(tim_num):
   """!
-  Doxygen style docstring for interrupt callback function
+  This timer interrupt runs every 10ms. It only fills the queue with data when doFillQueue is set to true.
   """
   #print("interrupting")
   if doFillQueue == True:
@@ -27,7 +30,7 @@ def timer_int(tim_num):
 
 def step_response ():
     """!
-    Doxygen style docstring for this function 
+    This function sets up the timer interrupt and output pin. It then pauses breifly before performing the step input and capturing data. Data capture is complete when the queue is full.
     """
     # Function code here
       
@@ -36,32 +39,38 @@ def step_response ():
     timmy.counter ()
     timmy.callback(timer_int)
 
-    
+    # Setup output pin
     pinA5 = pyb.Pin(pyb.Pin.board.PC0, pyb.Pin.OUT_PP)
 
+    # Set output pin to 0, and allow all transient response to settle before performing the step input
     pinA5.value(0)
     utime.sleep(3.0)
     pinA5.value(1)
     
     
+    # Begin capturing data by enabling doFillQueue
     print("Capturing...")
     global doFillQueue
     doFillQueue = True
     print(doFillQueue)
     
+    
     while int_queue.full() == False:
-        # do nothing and wait
+        # do nothing and wait for queue to fill
         ham = 0
         
     
     print("Capture Complete")
+    # Prevent the queue from being overwritten
     doFillQueue = False
     
-    i = 0;
-    for num in range(QUEUE_SIZE):
+    
+    # Loop over data collected in queue
+    for i, num in range(QUEUE_SIZE):
+        # Map adc values to voltage
         voltageRead = int_queue.get()/4096 * 3.3
+        # Create the string to print using csv format
         outString = str(i*10) + "," + str(voltageRead)
-        i += 1
         print(outString)
         
     print("End");
