4417b3f921b3e66480a416b40844fce92c2d2174
Mioshan <147013512+Mioshan@users.noreply.github.com>
2024-01-11 16:29:09 -0800

    Update step_response.py

--- src/step_response.py
+++ src/step_response.py
@@ -1,13 +1,14 @@
+"""! @file main.py
+This program runs a 3.3V step on output pin A5, and then records
+the immediate response on pin PB0 for the following 2 seconds.
+Data points from the recorded response are printed over serial.
+"""
+
 import cqueue
 import utime
 import micropython
 # Allocate memory to catch exceptions in interrupts
 micropython.alloc_emergency_exception_buf(100)
-
-
-"""! @file main.py
-   This program runs a 3.3V step on output pin A5, and then records the immediate response on pin PB0 for the following 2 seconds. Data points from the recorded response are printed over serial.
-"""
 
 # Setup queue for optimized data storage in an interrupt
 QUEUE_SIZE = 200
@@ -21,7 +22,8 @@ doFillQueue = False
 
 def timer_int(tim_num):
   """!
-  This timer interrupt runs every 10ms. It only fills the queue with data when doFillQueue is set to true.
+  This timer interrupt runs every 10ms.
+  It only fills the queue with data when doFillQueue is set to true.
   """
   #print("interrupting")
   if doFillQueue == True:
@@ -30,7 +32,8 @@ def timer_int(tim_num):
 
 def step_response ():
     """!
-    This function sets up the timer interrupt and output pin. It then pauses breifly before performing the step input and capturing data. Data capture is complete when the queue is full.
+    This function sets up the timer interrupt and output pin.
+    It then pauses breifly before performing the step input and capturing data. Data capture is complete when the queue is full.
     """
     # Function code here
       
@@ -52,8 +55,6 @@ def step_response ():
     print("Capturing...")
     global doFillQueue
     doFillQueue = True
-    print(doFillQueue)
-    
     
     while int_queue.full() == False:
         # do nothing and wait for queue to fill
@@ -66,7 +67,7 @@ def step_response ():
     
     
     # Loop over data collected in queue
-    for i, num in range(QUEUE_SIZE):
+    for i in range(QUEUE_SIZE):
         # Map adc values to voltage
         voltageRead = int_queue.get()/4096 * 3.3
         # Create the string to print using csv format
