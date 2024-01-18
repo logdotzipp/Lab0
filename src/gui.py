10d79e1289868cb7353b32d1397d033538245366
Mioshan <147013512+Mioshan@users.noreply.github.com>
2024-01-18 15:40:48 -0800

    Forgot a comma, lol

--- src/gui.py
+++ src/gui.py
@@ -112,7 +112,7 @@ def plot_data(plot_axes, plot_canvas,xvals,yvals,labels):
     plot_axes.clear()
     plot_canvas.draw()
     print("PC - Plotting Data...")
-    plot_axes.plot(xvals, yvals,'.' xth, yth)
+    plot_axes.plot(xvals, yvals,'.', xth, yth)
     plot_axes.set_xlabel(labels[0])
     plot_axes.set_ylabel(labels[1])
     plot_axes.grid(True)
