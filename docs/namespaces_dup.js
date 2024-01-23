49ced36768b619accaf6ccff99558f5045b717a0
Mioshan <147013512+Mioshan@users.noreply.github.com>
2024-01-23 13:58:51 -0800

    Second Update for Deoxygen
    Python programs have a description for what they do.

--- docs/namespaces_dup.js
+++ docs/namespaces_dup.js
@@ -3,8 +3,8 @@ var namespaces_dup =
     [ "gui", "namespacegui.html", [
       [ "plot_data", "namespacegui.html#a56ac65b573a69731ae8425d7e6b0c8f1", null ],
       [ "quitprgm", "namespacegui.html#a7fcbba55741ede77cb4a68802f3bb4c4", null ],
+      [ "rc_response", "namespacegui.html#ac58fb21ea2b6b18a0e2843ed12fd283b", null ],
       [ "send_message", "namespacegui.html#a37aaca38b8973d5a75cc788461c6a468", null ],
-      [ "test", "namespacegui.html#ae7a290b6717283bba9cc62fe7c5c403f", null ],
       [ "waitforstring", "namespacegui.html#a12a6174fc29cc4e9d1af732ffe099b6e", null ],
       [ "ser", "namespacegui.html#a7b0195188bc262f41d13a21afbbbbeb4", null ],
       [ "title", "namespacegui.html#aa02e4b026121c30cf2a201bd241417d2", null ]
